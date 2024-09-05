b94a8e2452cee14f58fc2a5cc5faa2c08f677c49
Carlos Verdugo <carlosverdugo@proton.me>
2024-09-04 22:23:10 -0700

    Adding form style

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.send('notes')
+router.get('/add', (req, res) => {
+    res.render('notes/new-notes')
 })
 
 module.exports = router; 

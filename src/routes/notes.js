5480a3ffc908798955044d994ad64ab06fa1c9f3
Carlos Verdugo <carlosverdugo@proton.me>
2024-09-04 22:30:37 -0700

    Handle errors new notes view

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -2,6 +2,27 @@ const router = require('express').Router();
 
 router.get('/add', (req, res) => {
     res.render('notes/new-notes')
-})
+});
+
+router.post('/new-note', (req, res) => {
+    console.log(req.body);
+    const { title, description } = req.body;
+    const errors = [];
+    if (!title) {
+        errors.push({text: 'Please write a title'})
+    }
+    if (!description) {
+        errors.push({text: 'Please write a description'})
+    }
+    if (errors.length > 0) {
+        res.render('notes/new-notes', { 
+            errors,
+            title,
+            description
+        });
+    } else {
+        res.send('ok');
+    }
+});
 
 module.exports = router; 
